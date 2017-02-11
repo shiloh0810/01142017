0dc5c7a66cc137ee821baafe4911a937472393b8
shiloh0810 <shilohliu@ShilohdeMacBook-Pro.local>
2017-02-11 22:41:06 +0800

    轉彎

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ var canvas = document.getElementById("game-canvas");
 
 // 取得 2D繪圖用的物件
 var ctx = canvas.getContext("2d");
+var newX;
 
 function draw(){
   enemy.move();
@@ -24,7 +25,9 @@ function draw(){
   ctx.drawImage(enemyImg,enemy.x,enemy.y);
   ctx.drawImage(buttonImg,640-80,480-80,80,80);
   if(isBuilding == true){
+
   	ctx.drawImage(defenseImg,cursor.x-cursor.x%32,cursor.y-cursor.y%32);
+
   }
   else
   {
@@ -32,14 +35,53 @@ function draw(){
   }
 }
 
+var enemyPath=[
+	{x:80, y:0},
+	{x: 80, y: 176},
+	{x: 336, y: 176},
+	{x: 336, y: 304},
+	{x: 144, y: 304},
+	{x: 144, y: 400},
+	{x: 528, y: 400},
+	{x: 528, y: 128},
+]
+
+
 var enemy = {
 	x: 80,
 	y : 0,
 	speedX : 0,
 	speedY : 64,
+	pathDes: 0,
 	move : function(){
-		this.x += this.speedX/FPS;
-		this.y += this.speedY/FPS;
+		
+		if(isCollided(enemyPath[this.pathDes].x, enemyPath[this.pathDes].y,
+				this.x, this.y, 64/FPS, 64/FPS)){
+			this.x=enemyPath[this.pathDes].x;
+			this.y=enemyPath[this.pathDes].y;
+			
+			this.pathDes++;
+			if(enemyPath[this.pathDes].y<this.y){
+				//up
+				this.speedX=0;
+				this.speedY=-64;
+			}else if(enemyPath[this.pathDes].x>this.x){
+				//right
+				this.speedX=64;
+				this.speedY=0;
+			}else if(enemyPath[this.pathDes].y>this.y){
+				//down
+				this.speedX=0;
+				this.speedY=64;
+			}else{
+				//left
+				this.speedX=-64;
+				this.speedY=0;
+			}
+		}else{
+			this.x += this.speedX/FPS;
+			this.y += this.speedY/FPS;
+		}
 	}
 }
 
@@ -73,6 +115,15 @@ function click(){
 function mousemove(event){
 	cursor.x = event.offsetX;
 	cursor.y = event.offsetY;
+}
+
+function isCollided(pointX, pointY, targetX, targetY, targetWidth, targetHeight){
+	if(targetX<=pointX && pointX<=targetX+targetWidth 
+		&& targetY<=pointY && pointY<=targetY+targetHeight){
+		return true;
+	}else{
+		return false;
+	}
 }
 // 執行 draw 函式
 setInterval(draw, 1000/FPS);
