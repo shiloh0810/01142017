a47f096c257f71691a1351145f190582623250d4
shiloh0810 <shilohliu@ShilohdeMacBook-Pro.local>
2017-03-11 20:36:46 +0800

    Lesson 17 and pause

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var FPS = 60;
 var clock = 0;
+var hp = 100;
+var con= true;
 // 創造 img HTML 元素，並放入變數中
 var bgImg = document.createElement("img");
 var slimeImg = document.createElement("img");
@@ -8,6 +10,7 @@ var jasonImg = document.createElement("img");
 var rukiaImg = document.createElement("img");
 var buttonImg = document.createElement("img");
 var defenseImg = document.createElement("img");
+var crossImg = document.createElement("img");
 
 // 設定這個元素的要顯示的圖片
 bgImg.src = "images/map.png";
@@ -17,6 +20,7 @@ jasonImg.src = "images/jason.gif";
 rukiaImg.src = "images/rukia.gif";
 buttonImg.src = "images/tower-btn.png";
 defenseImg.src = "images/tower.png";
+crossImg.src = "images/crosshair.png";
 
 // 找出網頁中的 canvas 元素
 var canvas = document.getElementById("game-canvas");
@@ -26,29 +30,64 @@ var ctx = canvas.getContext("2d");
 var newX;
 
 function draw(){
-  clock++;
-  if(clock%80==0)
+  if(con)
   {
-  	var newEnemy= new Enemy();
-  	enemies.push(newEnemy);
-  }
-  // 將背景圖片畫在 canvas 上的 (0,0) 位置
-  ctx.drawImage(bgImg,0,0);
-  for(i=0; i<enemies.length; i++)
-  {
-  	enemies[i].move();
-  	ctx.drawImage(pic[enemies[i].picture],enemies[i].x,enemies[i].y);
-  }
-  ctx.drawImage(buttonImg,640-80,480-80,80,80);
-  if(isBuilding){
+  	console.log(con)
+  	clock++;
+  	if(clock%80==0)
+	  {
+	  	var newEnemy= new Enemy();
+	  	enemies.push(newEnemy);
+	  }
+	  // 將背景圖片畫在 canvas 上的 (0,0) 位置
+	  ctx.drawImage(bgImg,0,0);
+	  for(i=0; i<enemies.length; i++)
+	  {
+	  	if(enemies[i].hp<=0)
+	  	{
+	  		enemies.splice(i, 1);
+	  	}
+	  	else
+	  	{
+	  		enemies[i].move();
+	  		ctx.drawImage(pic[enemies[i].picture],enemies[i].x,enemies[i].y);
+	  	}
+	  }
+	  ctx.drawImage(buttonImg,640-80,480-80,80,80);
+	  if(isBuilding){
 
-  	ctx.drawImage(defenseImg,cursor.x-cursor.x%32,cursor.y-cursor.y%32);
+	  	ctx.drawImage(defenseImg,cursor.x-cursor.x%32,cursor.y-cursor.y%32);
 
-  }
-  else
-  {
-  	ctx.drawImage (defenseImg, tower.x, tower.y)
-  }
+	  }
+	  else
+	  {
+	  	ctx.drawImage (defenseImg, tower.x, tower.y)
+	  }
+	  ctx.font="24px Arial";
+	  ctx.fillStyle="white";
+	  ctx.fillText("HP: "+hp, 10, 40);
+	  tower.searchEnemy();
+	  if(tower.aimingEnemyId!=null)
+	  {
+	  	var id = tower.aimingEnemyId;
+	  	ctx.drawImage (crossImg, enemies[id].x, enemies[id].y)
+	  }
+  } 
+}
+
+$("body").on("keypress", pause);
+function pause(event)
+{
+	console.log(event.which)
+	if(event.which == 112)
+	{
+		if(con){
+			con=false;
+		}
+		else{
+			con=true;
+		}
+	}
 }
 
 var enemyPath=[
@@ -70,13 +109,14 @@ var pic=[
 ]
 
 function Enemy(){
-	this.fast = 64+clock/20;
+	this.fast = 80+clock/20;
 	this.x = 80;
 	this.y = 0;
 	this.speedX = 0;
 	this.speedY = this.fast;
 	this.pathDes = 0;
 	this.picture = Math.floor((Math.random() * 4) + 0);
+	this.hp=10;
 	this.move = function(){
 		
 		if(isCollided(enemyPath[this.pathDes].x, enemyPath[this.pathDes].y,
@@ -86,6 +126,12 @@ function Enemy(){
 			this.y=enemyPath[this.pathDes].y;
 			//指定
 			this.pathDes++;
+			if(this.pathDes==enemyPath.length)
+			{
+				this.hp=0;
+				hp-=10;
+				return;
+			}
 			//計算，修改
 			if(enemyPath[this.pathDes].y<this.y){
 				//up
@@ -120,6 +166,21 @@ var cursor = {
 var tower = {
 	x: 0,
 	y: 0,
+	range: 96,
+	aimingEnemyId: null,
+	
+	searchEnemy: function(){
+	for(var i=0; i<enemies.length; i++){
+		var distance = Math.sqrt(Math.pow(this.x-enemies[i].x,2) + Math.pow(this.y-enemies[i].y,2));
+		if (distance<=this.range) {
+			this.aimingEnemyId = i;
+			return;
+		}
+	}
+	// 如果都沒找到，會進到這行，清除鎖定的目標
+	this.aimingEnemyId = null;
+	}
+	
 }
 
 $("#game-canvas").on("mousemove", mousemove);
