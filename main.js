c404e3f02514607e344e7bb1ed1407d5b957b5a1
shiloh0810 <shilohliu@ShilohdeMacBook-Pro.local>
2017-03-11 18:39:36 +0800

    Lesson 16

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 var FPS = 60;
+var clock = 0;
 // 創造 img HTML 元素，並放入變數中
 var bgImg = document.createElement("img");
-var enemyImg = document.createElement("img");
+var slimeImg = document.createElement("img");
+var daighImg = document.createElement("img");
+var jasonImg = document.createElement("img");
+var rukiaImg = document.createElement("img");
 var buttonImg = document.createElement("img");
 var defenseImg = document.createElement("img");
 
 // 設定這個元素的要顯示的圖片
 bgImg.src = "images/map.png";
-enemyImg.src = "images/slime.gif";
+slimeImg.src = "images/slime.gif";
+daighImg.src = "images/daigh.gif";
+jasonImg.src = "images/jason.gif";
+rukiaImg.src = "images/rukia.gif";
 buttonImg.src = "images/tower-btn.png";
 defenseImg.src = "images/tower.png";
 
@@ -19,12 +26,21 @@ var ctx = canvas.getContext("2d");
 var newX;
 
 function draw(){
-  enemy.move();
+  clock++;
+  if(clock%80==0)
+  {
+  	var newEnemy= new Enemy();
+  	enemies.push(newEnemy);
+  }
   // 將背景圖片畫在 canvas 上的 (0,0) 位置
   ctx.drawImage(bgImg,0,0);
-  ctx.drawImage(enemyImg,enemy.x,enemy.y);
+  for(i=0; i<enemies.length; i++)
+  {
+  	enemies[i].move();
+  	ctx.drawImage(pic[enemies[i].picture],enemies[i].x,enemies[i].y);
+  }
   ctx.drawImage(buttonImg,640-80,480-80,80,80);
-  if(isBuilding == true){
+  if(isBuilding){
 
   	ctx.drawImage(defenseImg,cursor.x-cursor.x%32,cursor.y-cursor.y%32);
 
@@ -46,36 +62,46 @@ var enemyPath=[
 	{x: 528, y: 128},
 ]
 
+var pic=[
+	slimeImg,
+	daighImg,
+	jasonImg,
+	rukiaImg,
+]
 
-var enemy = {
-	x: 80,
-	y : 0,
-	speedX : 0,
-	speedY : 64,
-	pathDes: 0,
-	move : function(){
+function Enemy(){
+	this.fast = 64+clock/20;
+	this.x = 80;
+	this.y = 0;
+	this.speedX = 0;
+	this.speedY = this.fast;
+	this.pathDes = 0;
+	this.picture = Math.floor((Math.random() * 4) + 0);
+	this.move = function(){
 		
 		if(isCollided(enemyPath[this.pathDes].x, enemyPath[this.pathDes].y,
-				this.x, this.y, 64/FPS, 64/FPS)){
+				this.x, this.y, this.fast/FPS, this.fast/FPS)){
+			//移動
 			this.x=enemyPath[this.pathDes].x;
 			this.y=enemyPath[this.pathDes].y;
-			
+			//指定
 			this.pathDes++;
+			//計算，修改
 			if(enemyPath[this.pathDes].y<this.y){
 				//up
 				this.speedX=0;
-				this.speedY=-64;
+				this.speedY= -this.fast;
 			}else if(enemyPath[this.pathDes].x>this.x){
 				//right
-				this.speedX=64;
+				this.speedX=this.fast;
 				this.speedY=0;
 			}else if(enemyPath[this.pathDes].y>this.y){
 				//down
 				this.speedX=0;
-				this.speedY=64;
+				this.speedY=this.fast;
 			}else{
 				//left
-				this.speedX=-64;
+				this.speedX=-this.fast;
 				this.speedY=0;
 			}
 		}else{
@@ -84,6 +110,8 @@ var enemy = {
 		}
 	}
 }
+
+var enemies = [];
 
 var cursor = {
 	x: 0,
